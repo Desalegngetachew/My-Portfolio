ffef5d187f050651d67d4a8c8e0fca546d80cbb6
desalegngetachew <desalegn251@gmail.com>
2024-07-10 04:23:40 -0300

    first commit

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   });
 
   var typed = new Typed(".typed", {
-    strings: ["Adobe suite tools", "JavaScript", "PHP","Flutter", "HTML", "Tailwind CSS", "React", "Vue","C++","MySQL","Java","MS offfice"],
+    strings: ["Adobe suite tools(Photoshop, illustrator,inDesign, priemer)", "JavaScript","capCut", "PHP","Flutter", "HTML", "Tailwind CSS", "React", "Vue","C++","MySQL","Java","MS offfice"],
     typeSpeed: 60,
     startDelay: 900,
     showCursor: false,
